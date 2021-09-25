53673119afc9e4874fc6e61ee60d242978fcda6e
Eatableninjago11 <83182611+Eatableninjago11@users.noreply.github.com>
2021-09-25 14:08:06 -0500

    Add files via upload

---
+++ screens/Ride.js
@@ -0,0 +1,144 @@
+   
+import React, { Component } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import * as Permissions from "expo-permissions";
+import { BarCodeScanner } from "expo-barcode-scanner";
+const bgImage = require("../assets/background2.png");
+const bicycleImg = require("../assets/bicycle.png");
+
+
+export default class RideScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      domState: "normal",
+      hasCameraPermissions: null,
+      scanned: false,
+      scannedData: ""
+    };
+  }
+
+  getCameraPermissions = async domState => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+
+    this.setState({
+      /*status === "granted" is true when user has granted permission
+          status === "granted" is false when user has not granted the permission
+        */
+      hasCameraPermissions: status === "granted",
+      domState: domState,
+      scanned: false
+    });
+  };
+
+  handleBarCodeScanned = async ({ type, data }) => {
+    this.setState({
+      scannedData: data,
+      domState: "normal",
+      scanned: true
+    });
+  };
+
+  render() {
+    const { domState, hasCameraPermissions, scannedData, scanned } = this.state;
+    if (domState === "scanner") {
+      return (
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : this.handleBarCodeScanned}
+          style={StyleSheet.absoluteFillObject}
+        />
+      );
+    }
+
+    return (
+      <View style={styles.container}>
+      <ImageBackground source={bgImage} style={styles.bgImage}>
+        <View style={styles.upperContainer}>
+          <Image source={bicycleImg} style={styles.bicycleImg} />
+          
+        </View>
+        <View style={styles.lowerContainer}>
+          <View style={styles.textinputContainer}>
+            <TextInput
+              style={styles.textinput}
+              placeholder={"Bike Id"}
+              placeholderTextColor={"#FFFFFF"}
+              value={bikeID}
+            />
+            <TouchableOpacity
+              style={styles.scanbutton}
+              onPress={() => this.getCameraPermissions("Bike Id")}
+            >
+              <Text style={styles.scanbuttonText}>Scan</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={[styles.textinputContainer, { marginTop: 25 }]}>
+            
+            
+              <Text style={styles.scanbuttonText}>Scan</Text>
+           
+          </View>
+        </View>
+      </ImageBackground>
+    </View>
+  );
+}
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#5653D4"
+  },
+  text: {
+    color: "#ffff",
+    fontSize: 15
+  },
+  button: {
+    width: "43%",
+    height: 55,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F48D20",
+    borderRadius: 15
+  },
+  buttonText: {
+    fontSize: 24,
+    color: "#FFFFFF"
+  },
+  textinputContainer: {
+    borderWidth: 2,
+    borderRadius: 10,
+    flexDirection: "row",
+    backgroundColor: "#9DFD24",
+    borderColor: "#FFFFFF"
+  },
+  textinput: {
+    width: "57%",
+    height: 50,
+    padding: 10,
+    borderColor: "#FFFFFF",
+    borderRadius: 10,
+    borderWidth: 3,
+    fontSize: 18,
+    backgroundColor: "#5653D4",
+    fontFamily: "Rajdhani_600SemiBold",
+    color: "#FFFFFF"
+  },
+  scanbutton: {
+    width: 100,
+    height: 50,
+    backgroundColor: "#9DFD24",
+    borderTopRightRadius: 10,
+    borderBottomRightRadius: 10,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  scanbuttonText: {
+    fontSize: 24,
+    color: "#0A0101",
+    fontFamily: "Rajdhani_600SemiBold"
+  }
+});
